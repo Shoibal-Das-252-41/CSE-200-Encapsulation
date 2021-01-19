e30378ded102f703c148b2d2bf5f47e440c01c00
Shoibal-Das-252-41 <shoibalshantu@gmail.com>
2021-01-19 21:56:58 +0600

    Second commit

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    class Account
+    {
+        int accountBalance = 858;
+
+        public int Balance
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Negative Balance Value Not Allowed!");
+                }
+                else 
+                {
+                    accountBalance = value;
+                }
+            }
+            get
+            {
+                return accountBalance;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account myAccount = new Account();
+            myAccount.Balance = 9759;
+            Console.WriteLine("Your Account Balance is : " + myAccount.Balance);
+            Console.ReadLine();
+        }
+    }
+}
